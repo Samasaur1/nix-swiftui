0559cd118a2e5a8f4364fad6e07b1c98faf26d1b
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-12-26 19:28:32 -0800

    swiftui example

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,41 @@
-print("Hello, world!")
+import Foundation
+import SwiftUI
+
+struct MyView: View {
+    @Environment(\.scenePhase) var scenePhase
+
+    var body: some View {
+        Text("# Hello")
+    }
+}
+struct MyScene: Scene {
+    @Environment(\.scenePhase) var scenePhase
+
+    var body: some Scene {
+        WindowGroup {
+            MyView()
+                .onAppear {
+                    print("onAppear")
+                    NSApplication.shared.activate(ignoringOtherApps: true)
+                }
+                .onDisappear {
+                    print("onDisappear")
+                }
+        }
+    }
+}
+struct MyApp: App {
+    @Environment(\.scenePhase) var scenePhase
+
+    var body: some Scene {
+        MyScene()
+    }
+}
+
+signal(SIGINT) { sig in
+    print() // clear line
+    exit(1)
+}
+
+NSApplication.shared.setActivationPolicy(.regular)
+MyApp.main()
